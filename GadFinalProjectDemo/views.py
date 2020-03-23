ce0608d59a8a9a24cf546ced0542b0abb3b76191
גד לידרור <gad.li1@tlvedu.org.il>
2020-03-23 11:13:28 +0200

    INtegrate the DataSet phase

--- GadFinalProjectDemo/views.py
+++ GadFinalProjectDemo/views.py
@@ -29,6 +29,11 @@ from datetime import datetime
 from os import path
 
 
+from GadFinalProjectDemo.Models.QueryFormStructure import QueryFormStructure 
+from GadFinalProjectDemo.Models.QueryFormStructure import LoginFormStructure 
+from GadFinalProjectDemo.Models.QueryFormStructure import UserRegistrationFormStructure 
+
+
 #### Subclasses spawn
 from GadFinalProjectDemo.Models.QueryFormStructure import QueryFormStructure 
 
@@ -79,30 +84,113 @@ def Album():
 def Query():
 
     Name = None
+    Country = ''
     capital = ''
     df = pd.read_csv(path.join(path.dirname(__file__), 'static\\Data\\capitals.csv'))
     df = df.set_index('Country')
+
+    raw_data_table = df.to_html(classes = 'table table-hover')
 
     form = QueryFormStructure(request.form)
      
     if (request.method == 'POST' ):
         name = form.name.data
-        
+        Country = name
         if (name in df.index):
             capital = df.loc[name,'Capital']
+            raw_data_table = ""
         else:
             capital = name + ', no such country'
-        ###form.name.data = ''
+        form.name.data = ''
 
-    raw_data_table = df.to_html(classes = 'table table-hover')
+
 
     return render_template('Query.html', 
             form = form, 
             name = capital, 
+            Country = Country,
             raw_data_table = raw_data_table,
             title='Query by the user',
             year=datetime.now().year,
             message='This page will use the web forms to get user input'
         )
+
+# -------------------------------------------------------
+# Register new user page
+# -------------------------------------------------------
+@app.route('/register', methods=['GET', 'POST'])
+def Register():
+    form = UserRegistrationFormStructure(request.form)
+
+    if (request.method == 'POST' and form.validate()):
+        if (not db_Functions.IsUserExist(form.username.data)):
+            db_Functions.AddNewUser(form)
+            db_table = ""
+
+            flash('Thanks for registering new user - '+ form.FirstName.data + " " + form.LastName.data )
+            # Here you should put what to do (or were to go) if registration was good
+        else:
+            flash('Error: User with this Username already exist ! - '+ form.username.data)
+            form = UserRegistrationFormStructure(request.form)
+
+    return render_template(
+        'register.html', 
+        form=form, 
+        title='Register New User',
+        year=datetime.now().year,
+        repository_name='Pandas',
+        )
+
+# -------------------------------------------------------
+# Login page
+# This page is the filter before the data analysis
+# -------------------------------------------------------
+@app.route('/login', methods=['GET', 'POST'])
+def Login():
+    form = LoginFormStructure(request.form)
+
+    if (request.method == 'POST' and form.validate()):
+        if (db_Functions.IsLoginGood(form.username.data, form.password.data)):
+            flash('Login approved!')
+            #return redirect('<were to go if login is good!')
+        else:
+            flash('Error in - Username and/or password')
+   
+    return render_template(
+        'login.html', 
+        form=form, 
+        title='Login to data analysis',
+        year=datetime.now().year,
+        repository_name='Pandas',
+        )
+
+
+
+@app.route('/DataModel')
+def DataModel():
+    """Renders the contact page."""
+    return render_template(
+        'DataModel.html',
+        title='This is my Data Model page abou UFO',
+        year=datetime.now().year,
+        message='In this page we will display the datasets we are going to use in order to answer ARE THERE UFOs'
+    )
+
+
+@app.route('/DataSet1')
+def DataSet1():
+
+    df = pd.read_csv(path.join(path.dirname(__file__), 'static\\Data\\capitals.csv'))
+    raw_data_table = df.to_html(classes = 'table table-hover')
+
+
+    """Renders the contact page."""
+    return render_template(
+        'DataSet1.html',
+        title='This is Data Set 1 page',
+        raw_data_table = raw_data_table,
+        year=datetime.now().year,
+        message='In this page we will display the datasets we are going to use in order to answer ARE THERE UFOs'
+    )
 
 
